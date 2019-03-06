98d453ac6369c198534901d5336426b131075593
Yunchun <chunix@foxmail.com>
2019-03-06 19:58:48 +0800

    Add Clion Project

---
+++ Algorithms/cmake-build-debug/CMakeFiles/Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algorithms.dir/main.c.o"
+  "Algorithms.pdb"
+  "Algorithms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
