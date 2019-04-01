aad0ada15d0f075f4ea8ded390cf8c22887f9b93
YUnchunZhu <chunix@foxmail.com>
2019-04-01 20:39:31 +0800

    021

--- Algorithms/021-MergeTwoSortedLists/mergeTwoSortedLists.c
+++ Algorithms/021-MergeTwoSortedLists/mergeTwoSortedLists.c
@@ -21,6 +21,7 @@ struct ListNode{
     struct ListNode *next;
 };
 
+/*
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
 {
     if(l1 == NULL && l2 == NULL)
@@ -66,7 +67,36 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
 
     return ls.next;
 }
+*/
 
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+    if(l1 == NULL)
+        return l2;
+
+    if(l2 == NULL)
+        return l1;
+
+    struct ListNode dummy;
+    struct ListNode *p = &dummy;
+
+    for(; l1 && l2; p = p->next)
+    {
+        if(l1->val < l2->val)
+        {
+            p->next = l1;
+            l1 = l1->next;
+        }
+        else
+        {
+            p->next = l2;
+            l2 = l2->next;
+        }
+    }
+    p->next = (l1 != NULL) ? l1 : l2;
+
+    return dummy.next;
+}
 
 int mergeTwoLists_main()
 {
